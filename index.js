737709256d9c458944c3e6181073508c2a470efa
kabornblack <shaibu.kabiru@email.com>
2023-04-05 21:28:48 +0300

    Creating routes and simple API

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 ///////////////////////////
 //FILES
@@ -32,8 +33,40 @@ const http = require("http");
 //SERVER
 
 const server = http.createServer((req, res) => {
-    res.end("Hello world from my server");
+
+    const pathName = req.url;
+
+    if (pathName === "/" || pathName === "/overview") {
+        res.end("This is the OVERVIEW");
+    } else if (pathName === "/product") {
+        res.end("This is the PRODUCT");
+    } else if (pathName === "/api") {
+
+        fs.readFile(`${__dirname}/starter/dev-data/data.json`, "utf-8", (err, data) => {
+            const productData = JSON.parse(data);
+            res.writeHead(200, {"Content-type": "application/json"});
+            res.end(data);
+        });
+        
+    } else {
+        res.writeHead(404, {
+            "Content-type": "text/html",
+            "my-own-header": "Hello world"
+        });
+        res.end("<h1>page not found!</h1>");
+    }
+    
 });
+
+
+
+
+
+
+
+
+
+
 
 server.listen(8000, "127.0.0.1", () => {
     console.log("Lestening to request on port 8000");
