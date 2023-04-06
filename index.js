64b4a3e107ae11100681d74e663c982c904f7ca5
kabornblack <shaibu.kabiru@email.com>
2023-04-06 21:40:56 +0300

    Add html and css to overview page and its looking good

--- index.js
+++ index.js
@@ -32,20 +32,52 @@ const url = require("url");
 ///////////////////////////////////
 //SERVER
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%NUTRIENT%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, "utf-8");
+
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, "utf-8");
-const dateObj = JSON.parse(data);
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-
     const pathName = req.url;
 
+    //Overview page
     if (pathName === "/" || pathName === "/overview") {
-        res.end("This is the OVERVIEW");
+        res.writeHead(200, {    "Content-type": "text/html"});
+
+        const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join("");
+        const output = tempOverview.replace(`{%PRODUCT_CARDS%}`, cardHtml);
+        res.end(output);
+
+
+
+
+
+
+
+    //Product page
     } else if (pathName === "/product") {
         res.end("This is the PRODUCT");
+
+    //API page
     } else if (pathName === "/api") {
         res.writeHead(200, {"Content-type": "application/json"});
         res.end(data);
+
+    //Page not found
     } else {
         res.writeHead(404, {
             "Content-type": "text/html",
@@ -53,7 +85,6 @@ const server = http.createServer((req, res) => {
         });
         res.end("<h1>page not found!</h1>");
     }
-    
 });
 
 
